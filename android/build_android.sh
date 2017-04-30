4d00e79649d82ecfcd3aee70909ed55780648504
Esteve Fernandez <esteve@apache.org>
2017-04-30 10:14:13 +0200

    Simplify CMake arguments

--- android/build_android.sh
+++ android/build_android.sh
@@ -1,7 +1,7 @@
 set -e
 
 ROS2_CURDIR=$PWD
-ROS2_JAVA_DIR=$(test -n $TRAVIS && echo /home/travis/build || echo $ROS2_CURDIR)
+ROS2_JAVA_DIR=$(test -n "$TRAVIS" && echo /home/travis/build || echo $ROS2_CURDIR)
 ROS2_OUTPUT_DIR=$ROS2_JAVA_DIR/output
 AMENT_WS=$ROS2_JAVA_DIR/ament_ws
 ROS2_ANDROID_WS=$ROS2_JAVA_DIR/ros2_android_ws
@@ -29,13 +29,13 @@ if [ -z "$ROS2_JAVA_SKIP_FETCH" ]; then
   wget https://raw.githubusercontent.com/esteve/ament_java/$ROS2_JAVA_BRANCH/ament_java.repos || wget https://raw.githubusercontent.com/esteve/ament_java/master/ament_java.repos
   vcs import $AMENT_WS/src < ament_java.repos
   vcs custom --git --args checkout $ROS2_JAVA_BRANCH || true
-  vcs export
+  vcs export --exact
 
   cd $ROS2_ANDROID_WS
   wget https://raw.githubusercontent.com/esteve/ros2_java/$ROS2_JAVA_BRANCH/ros2_java_android.repos || wget https://raw.githubusercontent.com/esteve/ros2_java/master/ros2_java_android.repos
   vcs import $ROS2_ANDROID_WS/src < ros2_java_android.repos || true
   vcs custom --git --args checkout $ROS2_JAVA_BRANCH || true
-  vcs export
+  vcs export --exact
 
   cd $ROS2_ANDROID_WS/src/ros2/rosidl
   touch python_cmake_module/AMENT_IGNORE
@@ -49,6 +49,9 @@ if [ -z "$ROS2_JAVA_SKIP_FETCH" ]; then
 #  cd $ROS2_ANDROID_WS/src/eProsima/Fast-RTPS
 #  git submodule init
 #  git submodule update
+#  if [ -n "$TRAVIS" ]; then
+#    find $ROS2_ANDROID_WS/src/ros2/examples/rclcpp $ROS2_ANDROID_WS/src/ros2/examples/rclpy -name "package.xml" -printf "%h\n" | xargs -i touch {}/AMENT_IGNORE
+#  fi
 fi
 
 cd $ROS2_ANDROID_WS/src/ros2/rmw_fastrtps
@@ -59,27 +62,17 @@ test -e $ROS2_ANDROID_WS/src/ros2/rmw_fastrtps/rmw_fastrtps_c/package.xml && tou
 
 if [ -z "$ROS2_JAVA_SKIP_AMENT" ]; then
   cd $AMENT_WS
-  $AMENT_WS/src/ament/ament_tools/scripts/ament.py build --symlink-install --isolated --install-space $AMENT_INSTALL_DIR --build-space $AMENT_BUILD_DIR
+  $AMENT_WS/src/ament/ament_tools/scripts/ament.py build --parallel --symlink-install --isolated --install-space $AMENT_INSTALL_DIR --build-space $AMENT_BUILD_DIR
 fi
 
 . $AMENT_INSTALL_DIR/local_setup.sh
 
 cd $ROS2_ANDROID_WS
-ament build --symlink-install --isolated --install-space $ROS2_ANDROID_INSTALL_DIR --build-space $ROS2_ANDROID_BUILD_DIR --cmake-args \
+ament build --parallel --symlink-install --isolated --install-space $ROS2_ANDROID_INSTALL_DIR --build-space $ROS2_ANDROID_BUILD_DIR --cmake-args \
   -DEPROSIMA_BUILD=ON \
   -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
   -DANDROID_FUNCTION_LEVEL_LINKING=OFF -DANDROID_NATIVE_API_LEVEL=$ANDROID_TARGET -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_STL=$ANDROID_STL \
   -DANDROID_ABI=$ANDROID_ABI -DANDROID_NDK=$ANDROID_NDK -DTHIRDPARTY=ON -DCOMPILE_EXAMPLES=OFF -DCMAKE_FIND_ROOT_PATH="$AMENT_INSTALL_DIR;$ROS2_ANDROID_INSTALL_DIR" \
-  -DANDROID_CPP_FEATURES="exceptions rtti" \
-  -DENABLE_RMW_FASTRTPS_C=ON \
-  -- \
-  --ament-cmake-args \
-  -DEPROSIMA_BUILD=ON \
-  -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
-  -DANDROID_FUNCTION_LEVEL_LINKING=OFF -DANDROID_NATIVE_API_LEVEL=$ANDROID_TARGET -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_STL=$ANDROID_STL \
-  -DANDROID_ABI=$ANDROID_ABI -DANDROID_NDK=$ANDROID_NDK -DTHIRDPARTY=ON -DCOMPILE_EXAMPLES=OFF -DCMAKE_FIND_ROOT_PATH="$AMENT_INSTALL_DIR;$ROS2_ANDROID_INSTALL_DIR" \
-  -DANDROID_CPP_FEATURES="exceptions rtti" \
-  -DENABLE_RMW_FASTRTPS_C=ON \
   -- \
   --ament-gradle-args \
   -Pament.android_stl=$ANDROID_STL -Pament.android_abi=$ANDROID_ABI -Pament.android_ndk=$ANDROID_NDK -- $@
--- java/build_java.sh
+++ java/build_java.sh
@@ -1,7 +1,7 @@
 set -e
 
 ROS2_CURDIR=$PWD
-ROS2_JAVA_DIR=$(test -n $TRAVIS && echo /home/travis/build || echo $ROS2_CURDIR)
+ROS2_JAVA_DIR=$(test -n "$TRAVIS" && echo /home/travis/build || echo $ROS2_CURDIR)
 ROS2_OUTPUT_DIR=$ROS2_JAVA_DIR/output
 AMENT_WS=$ROS2_JAVA_DIR/ament_ws
 ROS2_JAVA_WS=$ROS2_JAVA_DIR/ros2_java_ws
@@ -29,18 +29,19 @@ if [ -z "$ROS2_JAVA_SKIP_FETCH" ]; then
   wget https://raw.githubusercontent.com/esteve/ament_java/$ROS2_JAVA_BRANCH/ament_java.repos || wget https://raw.githubusercontent.com/esteve/ament_java/master/ament_java.repos
   vcs import $AMENT_WS/src < ament_java.repos
   vcs custom --git --args checkout $ROS2_JAVA_BRANCH || true
-  vcs export
+  vcs export --exact
 #  vcs custom --git --args rebase origin/master || true
 
   cd $ROS2_JAVA_WS
   if [ -z "$TRAVIS" ]; then
     wget https://raw.githubusercontent.com/esteve/ros2_java/$ROS2_JAVA_BRANCH/ros2_java_desktop.repos || wget https://raw.githubusercontent.com/esteve/ros2_java/master/ros2_java_desktop.repos
+    vcs import $ROS2_JAVA_WS/src < ros2_java_desktop.repos
   else
     wget https://raw.githubusercontent.com/esteve/ros2_java/$ROS2_JAVA_BRANCH/ros2_java_desktop_travis.repos || wget https://raw.githubusercontent.com/esteve/ros2_java/master/ros2_java_desktop_travis.repos
+    vcs import $ROS2_JAVA_WS/src < ros2_java_desktop_travis.repos
   fi
-  vcs import $ROS2_JAVA_WS/src < ros2_java_desktop.repos
   vcs custom --git --args checkout $ROS2_JAVA_BRANCH || true
-  vcs export
+  vcs export --exact
   cd src/ros2
 #  vcs custom --git --args rebase origin/master || true
 
@@ -50,17 +51,21 @@ if [ -z "$ROS2_JAVA_SKIP_FETCH" ]; then
   else
     patch -p1 < ../../ros2_java/ros2_java/rosidl_typesupport_ros2_java_travis.patch
   fi
+
+  if [ -n "$TRAVIS" ]; then
+    find $ROS2_JAVA_WS/src/ros2/examples/rclcpp $ROS2_JAVA_WS/src/ros2/examples/rclpy -name "package.xml" -printf "%h\n" | xargs -i touch {}/AMENT_IGNORE
+  fi
 fi
 
 if [ -z "$ROS2_JAVA_SKIP_AMENT" ]; then
   cd $AMENT_WS
-  $AMENT_WS/src/ament/ament_tools/scripts/ament.py build --symlink-install --isolated --install-space $AMENT_INSTALL_DIR --build-space $AMENT_BUILD_DIR
+  $AMENT_WS/src/ament/ament_tools/scripts/ament.py build --parallel --symlink-install --isolated --install-space $AMENT_INSTALL_DIR --build-space $AMENT_BUILD_DIR
 fi
 
 . $AMENT_INSTALL_DIR/local_setup.sh
 
 cd $ROS2_JAVA_WS
-ament build --symlink-install --isolated --install-space $ROS2_JAVA_INSTALL_DIR --build-space $ROS2_JAVA_BUILD_DIR --cmake-args -DEPROSIMA_BUILD=ON -- $@
+ament build --parallel --symlink-install --isolated --install-space $ROS2_JAVA_INSTALL_DIR --build-space $ROS2_JAVA_BUILD_DIR $@
 
 if [ -z "$ROS2_JAVA_SKIP_TESTS" ]; then
   . $ROS2_JAVA_INSTALL_DIR/local_setup.sh
